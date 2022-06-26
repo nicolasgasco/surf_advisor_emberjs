40d76048e0dcb66ca12b57e0616d675d7b8cb128
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-06-26 23:30:49 +0200

    Add language selector to top navbar and move social badges

---
+++ app/components/layout/nav-bar/language-selector.js
@@ -0,0 +1,17 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
+import { action } from '@ember/object';
+
+export default class LanguageSelectorComponent extends Component {
+    @service intl;
+    @service router;
+
+    @tracked selectedLang = 'en';
+
+    @action
+    setSelectedLang(event) {
+        this.selectedLang = event.target.value;
+        this.router.transitionTo(`/${this.selectedLang}`);
+    }
+}
