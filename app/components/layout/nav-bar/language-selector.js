9b016f91e0bbe6758fdfe929d0ebf6235fe540ed
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-06-27 22:55:39 +0200

    Tweaked logic and styles of Language selector

--- app/components/layout/nav-bar/language-selector.js
+++ app/components/layout/nav-bar/language-selector.js
@@ -4,14 +4,14 @@ import { service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class LanguageSelectorComponent extends Component {
-    @service intl;
-    @service router;
+  @service intl;
+  @service router;
 
-    @tracked selectedLang = 'en';
+  @tracked selectedLang = 'en';
 
-    @action
-    setSelectedLang(event) {
-        this.selectedLang = event.target.value;
-        this.router.transitionTo(`/${this.selectedLang}`);
-    }
+  @action
+  setSelectedLang(event) {
+    this.selectedLang = event.target.value;
+    this.router.transitionTo(`/${this.selectedLang}`);
+  }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,25 +2,25 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class IndexRoute extends Route {
-    @service intl;
+  @service intl;
 
-    beforeModel() {
-        this.intl.setLocale(['es-us']);
-    }
+  beforeModel() {
+    this.intl.setLocale(['es-us']);
+  }
 
-    model(params) {
-        switch (params.lang) {
-            case 'en':
-                this.intl.setLocale(['en-us']);
-                document.documentElement.setAttribute('lang', 'en')
-                break;
-            case 'es':
-                this.intl.setLocale(['es-es']);
-                document.documentElement.setAttribute('lang', 'es')
-                break;
-            default:
-                this.intl.setLocale(['en-us']);
-                document.documentElement.setAttribute('lang', 'en')
-        }
+  model(params) {
+    switch (params.lang) {
+      case 'en':
+        this.intl.setLocale(['en-us']);
+        document.documentElement.setAttribute('lang', 'en');
+        break;
+      case 'es':
+        this.intl.setLocale(['es-es']);
+        document.documentElement.setAttribute('lang', 'es');
+        break;
+      default:
+        this.intl.setLocale(['en-us']);
+        document.documentElement.setAttribute('lang', 'en');
     }
+  }
 }
