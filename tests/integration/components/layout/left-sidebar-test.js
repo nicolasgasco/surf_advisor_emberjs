a29b4f27566541e1632eea32c53d74d584085229
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-06-29 23:01:28 +0200

    Add tests for Layout::LeftSidebar

--- tests/integration/components/layout/left-sidebar-test.js
+++ tests/integration/components/layout/left-sidebar-test.js
@@ -3,24 +3,13 @@ import { setupRenderingTest } from 'surf-advisor-emberjs/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | layout/left-sidebar', function (hooks) {
+module('Integration | Component | <Layout::LeftSidebar/>', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<Layout::LeftSidebar />`);
 
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Layout::LeftSidebar>
-        template block text
-      </Layout::LeftSidebar>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('[data-test-left-sidebar]').exists();
+    assert.dom('[data-test-left-sidebar-img="svg"]').hasAria('hidden', 'true');
   });
 });
