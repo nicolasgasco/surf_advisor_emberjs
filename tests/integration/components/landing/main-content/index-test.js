2e625d672c5e2a1ea6e2ba7b096056cbaae4d292
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-06-29 22:56:14 +0200

    Add tests for Landing::MainContent

--- tests/integration/components/landing/main-content/index-test.js
+++ tests/integration/components/landing/main-content/index-test.js
@@ -1,29 +1,22 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'surf-advisor-emberjs/tests/helpers';
+import { setupIntl, t } from 'ember-intl/test-support';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module(
-  'Integration | Component | landing/main-content/index',
-  function (hooks) {
-    setupRenderingTest(hooks);
+module('Integration | Component | <Landing::MainContent/>', function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks);
 
-    test('it renders', async function (assert) {
-      // Set any properties with this.set('myProperty', 'value');
-      // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders', async function (assert) {
+    await render(hbs`<Landing::MainContent />`);
 
-      await render(hbs`<Landing::MainContent::Index />`);
-
-      assert.dom(this.element).hasText('');
-
-      // Template block usage:
-      await render(hbs`
-      <Landing::MainContent::Index>
-        template block text
-      </Landing::MainContent::Index>
-    `);
-
-      assert.dom(this.element).hasText('template block text');
-    });
-  }
-);
+    assert.dom('[data-test-landing-main-content-wrapper]').exists();
+    assert.dom('[data-test-landing-main-content-title]').hasText(
+      t('index.title')
+        .replaceAll(/<\/*span>/g, '')
+        .replaceAll(/<br\/>/g, '')
+    );
+    assert.dom('[data-test-spots-selection-wrapper]').exists();
+  });
+});
