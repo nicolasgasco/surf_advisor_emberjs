5aaee0fc5bf32928164d7436a0ba63d07178c86c
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-07-03 16:07:19 +0200

    Refactor Form components

--- tests/integration/components/shared/form/option-test.js
+++ tests/integration/components/shared/form/option-test.js
@@ -7,14 +7,28 @@ module('Integration | Component | <Shared::Form::Option/>', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders as selected when the value is also the selected value', async function (assert) {
-    await render(hbs`<Shared::Form::Option @value="value" @selectedValue="value" @aria-label="aria" @label="label"/>`);
+    await render(
+      hbs`<Shared::Form::Option @value="value" @selectedValue="value" @aria-label="aria" @label="label"/>`
+    );
 
-    assert.dom('[data-test-form-option-selected]').hasAria('selected', 'true').hasAria('label', 'aria').hasText('label').hasAttribute('selected');
+    assert
+      .dom('[data-test-form-option-selected]')
+      .hasAria('selected', 'true')
+      .hasAria('label', 'aria')
+      .hasText('label')
+      .hasAttribute('selected');
   });
 
   test('it renders as unselected when the value is not the selected value', async function (assert) {
-    await render(hbs`<Shared::Form::Option @value="value" @selectedValue="other" @aria-label="aria" @label="label"/>`);
+    await render(
+      hbs`<Shared::Form::Option @value="value" @selectedValue="other" @aria-label="aria" @label="label"/>`
+    );
 
-    assert.dom('[data-test-form-option-unselected]').hasAria('selected', 'false').hasAria('label', 'aria').hasText('label').hasNoAttribute('selected');
+    assert
+      .dom('[data-test-form-option-unselected]')
+      .hasAria('selected', 'false')
+      .hasAria('label', 'aria')
+      .hasText('label')
+      .hasNoAttribute('selected');
   });
 });
--- tests/integration/components/shared/form/select-test.js
+++ tests/integration/components/shared/form/select-test.js
@@ -7,11 +7,18 @@ module('Integration | Component | <Shared::Form::Select/>', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    await render(hbs`<Shared::Form::Select @aria-label="aria" @label="label"/>`);
+    await render(
+      hbs`<Shared::Form::Select @aria-label="aria" @label="label"/>`
+    );
 
     assert.dom('[data-test-form-select-wrapper]').exists();
     assert.dom('[data-test-form-select]').hasAria('label', 'aria');
-    assert.dom('[data-test-form-default-option]').hasText("label").hasAttribute('selected');
-    assert.dom('[data-test-form-select-chevron="svg"]').hasAria('hidden', 'true');
+    assert
+      .dom('[data-test-form-default-option]')
+      .hasText('label')
+      .hasAttribute('selected');
+    assert
+      .dom('[data-test-form-select-chevron="svg"]')
+      .hasAria('hidden', 'true');
   });
 });
